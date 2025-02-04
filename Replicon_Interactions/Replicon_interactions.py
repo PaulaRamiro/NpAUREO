363feb71d1e495ae674b68b61bfe45680bcb757f
Paula Ramiro <152322543+PaulaRamiro@users.noreply.github.com>
2025-02-04 11:39:55 +0100

    Update Replicon_interactions.py

--- Replicon_Interactions/Replicon_interactions.py
+++ Replicon_Interactions/Replicon_interactions.py
@@ -5,32 +5,32 @@ import os
 
 
 def read_and_transform_csv(directory):
-    # Obtener la lista de todos los archivos CSV en el directorio
+    # Get the list of all CSV files in the directory
     csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
 
-    # Leer archivo CSV y convertir en DataFrames
+    # Read CSV file and convert it into DataFrames
     for file in csv_files:
         file_path = os.path.join(directory, file)
         df = pd.read_csv(file_path, sep='\t')
 
-    # Multiple_replicons to bool column
+    # Convert Multiple_replicons to boolean column
     df.loc[df['Multiple_replicons'] == 'Multiple replicons', 'Multiple_replicons'] = True
     df.loc[df['Multiple_replicons'] == 'Single replicons', 'Multiple_replicons'] = False
 
-    # Filter undetected plasmids
+    # Filter out undetected plasmids
     df = df[df['rep_type.s.'] != '-']
 
     df_og = df.copy()
     df_og = df_og.rename(columns={'rep_type.s.': 'plasmids'})
 
-    # Filter only those plasmids which appear ar least 3 different times
+    # Filter only those plasmids that appear at least 3 different times
     conteo = df_og.groupby('plasmids')['PCN'].count().reset_index()
     repes = list(conteo[conteo['PCN'] >= 3]['plasmids'])
     df_filtered = df_og[df_og['plasmids'].isin(repes)]
     df_filtered['plasmids'] = df_filtered['plasmids'].str.split(',')
 
-    # Create a dictionary where each key is a possible combination of plasmid replicons and each value is a dataframe
-    # containing every element of the key, separated or combined
+    # Create a dictionary where each key is a possible combination of plasmid replicons 
+    # and each value is a dataframe containing every element of the key, separated or combined
     df_cross = pd.DataFrame(columns=df_filtered.columns)
     df_cross['replicon_ID'] = pd.Series(dtype='str')
     for i in repes:
@@ -51,18 +51,19 @@ def read_and_transform_csv(directory):
 
 
 def main():
-    # Directorio donde se encuentran los archivos CSV
+    # Directory where the CSV files are located
     csv_directory = 'src'
 
-    # Leer y combinar los archivos CSV
+    # Read and combine the CSV files
     combined_df = read_and_transform_csv(csv_directory)
 
-    # Guardar el DataFrame combinado en un nuevo archivo CSV
+    # Save the combined DataFrame into a new CSV file
     output_path = os.path.join(csv_directory, 'Plasmid_interactions_combined.csv')
     combined_df.to_csv(output_path, index=False)
 
-    print(f'Todos los archivos CSV han sido combinados en {output_path}')
+    print(f'All CSV files have been combined into {output_path}')
 
 
 if __name__ == "__main__":
     main()
+
